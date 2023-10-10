dfe0956e67c9d17c9e0d9f9315d9afa1fc2feb66
Sarah-Gad <sarahgad298@gmail.com>
2023-10-10 15:14:45 -0700

    completed task n.0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Enrty point of the program
+ *
+ * Description: this program prints
+ * _putchar, followed by a new line.
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
