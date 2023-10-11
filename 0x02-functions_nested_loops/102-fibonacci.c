e8241df87c44cf0955acaddfb8b4a73be00b1d2b
Sarah-Gad <sarahgad298@gmail.com>
2023-10-10 19:49:41 -0700

    completed task n.14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,19 +10,21 @@
 
 int main(void)
 {
-	unsigned long fi1 = 0, fi2 = 1, sum;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 	float total_sum;
 
 	while (1)
 	{
-		sum = fi1 + fi2;
+		sum = fib1 + fib2;
+
 		if (sum > 4000000)
 			break;
+
 		if ((sum % 2) == 0)
 			total_sum += sum;
 
-		fi1 = fi2;
-		fi2 = sum;
+		fib1 = fib2;
+		fib2 = sum;
 	}
 	printf("%.0f\n", total_sum);
 
