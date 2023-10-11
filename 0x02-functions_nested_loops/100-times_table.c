844f40b655a0eac56376cf541c8be32eef288849
Sarah-Gad <sarahgad298@gmail.com>
2023-10-10 19:15:47 -0700

    completed task n.12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ void print_times_table(int n)
 					_putchar((num3 / 100) + 48);
 					_putchar((num3 / 10) + 48);
 				}
-				elsr if (num3 <= 99 && num3 >= 10)
+				else if (num3 <= 99 && num3 >= 10)
 					_putchar((num3 / 10) + 48);
 				_putchar((num3 % 10) + 48);
 			}
