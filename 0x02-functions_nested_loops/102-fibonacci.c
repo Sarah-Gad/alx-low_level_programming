76fbda0f03fc7d03e733ecf58cf41d21fa9224cd
Sarah-Gad <sarahgad298@gmail.com>
2023-10-10 20:42:20 -0700

    completed task n.16

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,31 +2,29 @@
 /**
  * main - Entry point
  *
- * Description: prints the first 50 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line.
+ * Description: a program that prints
+ * the first 50 Fibonacci numbers
  *
  * Return: Always 0 (success)
 */
 
 int main(void)
 {
+	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	float total_sum;
 
-	while (1)
+	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
-
-		if (sum > 4000000)
-			break;
-
-		if ((sum % 2) == 0)
-			total_sum += sum;
+		printf("%lu", sum);
 
 		fib1 = fib2;
 		fib2 = sum;
-	}
-	printf("%0.0f\n", total_sum);
 
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
 	return (0);
 }
