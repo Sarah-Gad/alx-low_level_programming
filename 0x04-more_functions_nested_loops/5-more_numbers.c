63453d1cb3ecd4aa45aa80dcb27dca2e44491dc5
Sarah-Gad <sarahgad298@gmail.com>
2023-10-12 15:56:29 -0700

    completed task n.5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,21 @@
 void more_numbers(void)
 {
 	int num_l;
+	int num_c;
 	int nums;
 
 	for (num_l = 0; num_l < 10; num_l++)
 	{
-		for (nums = 0; nums < 15; nums++)
+		for (num_c = 0; num_c <= 14; num_c++)
 		{
-			;
+			nums = num_c;
+			if (num_c > 9)
+			{
+				_putchar(1 + 48);
+				nums = num_c % 10;
+			}
+			_putchar(nums + 48);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
-	putchar('\n');
 }
