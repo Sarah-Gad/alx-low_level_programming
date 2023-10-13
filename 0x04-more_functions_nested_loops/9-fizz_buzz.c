d376a8fbfd9153a86f8d5bdf02e89722212a0f00
Sarah-Gad <sarahgad298@gmail.com>
2023-10-12 18:39:22 -0700

    completed task n.9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,21 +20,28 @@ int main(void)
 	{
 		if ((num % 3 == 0) && (num % 5 != 0))
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((num % 5 == 0) && (num % 3 != 0))
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", num);
+			printf("%d", num);
+		}
+		if (num != 100)
+		{
+			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
 		}
 	}
-	putchar('\n');
 	return (0);
 }
