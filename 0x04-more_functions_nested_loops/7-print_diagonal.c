edd4462e750e4c41bc7a3f3407a4ccc0d903b9d8
Sarah-Gad <sarahgad298@gmail.com>
2023-10-12 17:09:10 -0700

    completed task n.7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,7 @@
 void print_diagonal(int num)
 {
 	int l_num;
+	int s_num;
 
 	if (num == 0 || num < 0)
 	{
@@ -18,6 +19,10 @@ void print_diagonal(int num)
 	{
 		for (l_num = 1; l_num <= num; l_num++)
 		{
+			for (s_num = 1; s_num < l_num; s_num++)
+			{
+				_putchar(' ');
+			}
 			_putchar(92);
 			_putchar('\n');
 		}
