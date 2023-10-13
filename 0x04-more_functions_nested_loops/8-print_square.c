6aa67834d95e9ecaaa2296b2b9c826b8a9695c56
Sarah-Gad <sarahgad298@gmail.com>
2023-10-12 18:00:26 -0700

    completed task n.8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,6 +11,11 @@ void print_square(int size)
 	int row;
 	int column;
 
+	if (size == 0 || size < 0)
+	{
+		_putchar('\n');
+	}
+
 	for (row = 1; row <= size; row++)
 	{
 		for (column = 1; column <= size; column++)
