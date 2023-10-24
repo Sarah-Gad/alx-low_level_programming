15fafcba2d18082b82276a3e937b15fad5c18399
Sarah-Gad <sarahgad298@gmail.com>
2023-10-23 18:54:44 -0700

    completed task n.5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - a function
+ * that locates a substring.
+ *
+ * @haystack: the string to search in
+ * @needle: the srting to search for
+ *
+ * Return: a pointer or null.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+	int b;
+
+	for (a = 0; needle[a] != '\0'; a++)
+	{
+		for (b = 0; haystack[b] != '\0'; b++)
+		{
+			if (needle[a] == haystack[b])
+			{
+				return (needle + a);
+			}
+		}
+	}
+	return ('\0');
+}
