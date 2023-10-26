776799125a3ac3c0c02924aa161e96c63a9db4cf
Sarah-Gad <sarahgad298@gmail.com>
2023-10-25 19:42:48 -0700

    completed task n.7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,26 +1,73 @@
-#include <string.h>
 #include "main.h"
+int str_r(char *s);
+int hlp(char *strt, char *end);
 
 /**
- * is_palindrome - a function that returns 1 if a string
- * is a palindrome and 0 if not
- * @s: pointer indicates to the string
- * Return: one or zero
+ * is_hlpndrome - a function that returns 1
+ * if a string is a hlpndrome and 0 if not.
+ *
+ * @s: '*s' is a pointer
+ *
+ * Return: Always 0.
 */
 
-int is_palindrome(char *s)
+int is_hlpndrome(char *s)
 {
-	if (s == NULL || *s == '\0' || *(s + 1) == '\0')
-	{
+	int length;
+
+	char *end = s;
+	int i;
+
+	length = str_r(s);
+	i = hlp(s, end + (length - 1));
+	return (i);
+}
+
+/**
+ * hlp - a helper func
+ *
+ * @strt: '*strt' is a pointer
+ * @end: '*end' is a pointer
+ *
+ * Return: Always 0.
+*/
+
+int hlp(char *strt, char *end)
+{
+	int i;
+
+	if (strt >= end)
 		return (1);
-	}
-	else if (*s != *(s + strlen(s) - 1))
-	{
+	if (*strt != *end)
 		return (0);
+	if
+	{
+		i = hlp(strt + 1, end - 1);
+		return (i);
+	}
+}
+
+/**
+ * str_r - another helper func
+ *
+ * @s: '*s' is a pointer
+ *
+ * Return: Always 0.
+*/
+
+int str_r(char *s)
+{
+	int i;
+
+	if (*s != '\0')
+	{
+		i = str_r(s + 1);
+		i++;
+		return (i);
 	}
 	else
 	{
-		*(s + strlen(s) - 1) = '\0';
-		return (is_palindrome(s + 1));
+		i = 0;
+		return (i);
 	}
 }
