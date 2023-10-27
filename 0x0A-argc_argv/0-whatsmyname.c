17de29b96515b0d86730446cd561b6995e71c8f6
Sarah-Gad <sarahgad298@gmail.com>
2023-10-27 14:24:09 -0700

    completed task n.0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - entry point of the program.
+ *
+ * @argc: number of arguments in the command line.
+ * @argv: array of string
+ *
+ * Return: 0 always success.
+*/
 
 int main(int argc, char *argv[])
 {
