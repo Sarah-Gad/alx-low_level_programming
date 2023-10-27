5b080e132b0ee0ab5429a18489eb9ae5de93be8d
Sarah-Gad <sarahgad298@gmail.com>
2023-10-27 14:43:39 -0700

    completed task n.1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of the program
+ *
+ * @argc: number of arguments passed to
+ * the main function
+ *
+ * @argv: pointer to the arguments passed
+ * to the main function
+ *
+ * Return: 0 always success
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
