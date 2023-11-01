34d7f6ac79afdbe625576de75cd0216235518ec7
Sarah-Gad <sarahgad298@gmail.com>
2023-10-31 19:28:31 -0700

    completed task n.6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,44 +1,127 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- *argstostr - Concatenates all the arguments of the program.
- *@ac: The number of arguments.
- *@av: An array of strings containing the arguments.
- *Return: A pointer to the con string, or NULL if it fails.
+ * letters_count - fucntion that is mintioned in another code
+ *
+ * Description: function to do task for alx
+ *
+ * @str: '*s' is a pointer
+ * @size: 'size' is the size of the string
+ *
+ * Return: Always 0.
 */
-char *argstostr(int ac, char **av)
+
+int letters_count(char *str, int size)
 {
-	int tot_l = 0;
-	int i, j;
-	int indx = 0;
+	int i;
+	int sw = 0, j;
 
-	char *con;
-
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		if (str[i] != ' ')
 		{
-			tot_l++;
+			j = i + 1;
+			while (str[j] != ' ' && str[j] != '\0')
+			{
+				j++;
+			}
+			sw++;
+			i = j - 1;
 		}
-		tot_l++;
 	}
-	con = malloc((tot_l + 1) * sizeof(char));
+	return (sw);
+}
 
-	if (con == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
+/**
+ * word_count - fucntion that is mintioned in another code
+ *
+ * Description: function to do task for alx
+ *
+ * @str: '*s' is a pointer
+ * @size: 'size' is the size of the string
+ *
+ * Return: Always 0.
+*/
+
+int word_count(char *str, int size)
+{
+	int i, words = 0;
+
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			con[indx] = av[i][j];
-			indx++;
-		}
-		con[indx] = '\n';
-		indx++;
+		if (str[i] == ' ' && str[i + 1] != ' ' && (i + 1) < size)
+			words++;
 	}
-	con[indx] = '\0';
-	return (con);
+	return (words);
+}
+
+/**
+ * _strlen - fucntion that is mintioned in another code
+ *
+ * Description: function to do task for alx
+ *
+ * @s: '*s' is a pointer
+ *
+ * Return: Always 0.
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+	while (s[len] != 0)
+		len++;
+	return (len);
+}
+
+/**
+ * strtow - function is called ny another file called main.c
+ *
+ * Description: for alx project malloc
+ *
+ * @str: '*s1' is a pointer recieved from another function
+ *
+ * Return: 0 on success
+*/
+
+char **strtow(char *str)
+{
+	int size, i, j, x, y, letters;
+	int flag = 0;
+	char **ptr;
+	if (str == NULL || *str == '\0' || (*str == ' ' && str[1] == '\0'))
+		return (NULL);
+	while (*str != '\0' && *str == ' ')
+		str++;
+	size = _strlen(str);
+	letters = letters_count(str, size) + 1;
+	ptr = (char **)malloc(letters *sizeof(char *));
+	if (ptr == NULL)
+		return (NULL);
+	ptr[letters - 1] = NULL;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ')
+		{
+			for (j = 0; str[i + j] != ' ' && str[i + j] != '\0'; j++)
+				;
+			ptr[flag] = (char *)malloc((j + 1) * sizeof(char));
+			if (ptr[flag] == NULL)
+			{
+				for (x = 0; x < flag; x++)
+					free(ptr[x]);
+				free(ptr);
+				return (NULL);
+			}
+			for (y = 0; y < j; y++)
+				ptr[flag][y] = str[i + y];
+			ptr[flag][y] = '\0';
+			flag++;
+			i += j;
+		}
+		else
+			i++;
+	}
+	return (ptr);
 }
