a102265f9ce1df0ff41d0a8079889a9cb1533ad9
Sarah-Gad <sarahgad298@gmail.com>
2023-11-08 18:20:37 -0800

    completed task n.4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * main - check the code
+ * @argc: counter of arguments
+ * @argv: values of argumens
+ * Return: Always 0.
+*/
+int main(int argc, char **argv)
+{
+	int buffer;
+	char *ptr = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	buffer = atoi(argv[1]);
+	if (buffer < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (buffer)
+	{
+		buffer--;
+		printf("%02hhx", *ptr);
+		ptr++;
+		if (buffer)
+		{
+			printf(" ");
+		}
+		else
+			printf("\n");
+	}
+	return (0);
+}
