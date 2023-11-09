da30b43fb455cc9659627c030e74b1b5fa95bcf2
Sarah-Gad <sarahgad298@gmail.com>
2023-11-08 16:16:22 -0800

    completed task n.1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - a function that executes
+ * a function given as a parameter on each
+ * element of an array.
+ * @array: pointer to the array
+ * @size: size of the array
+ * @action: pointer to the function.
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t indx;
+
+
+	if (array && size && action)
+	{
+		for (indx = 0; indx < size; indx++)
+		{
+			(*action)(array[indx]);
+		}
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,6 +6,5 @@
 
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
-void print_name_as_is(char *name);
-void print_name_uppercase(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
