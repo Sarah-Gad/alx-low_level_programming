cb8a5d692b209f12bb8ff6508f3bf0db5d862db0
Sarah-Gad <sarahgad298@gmail.com>
2023-11-09 17:18:35 -0800

    completed task n.2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,19 +10,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int indx;
 	unsigned int ind;
 	va_list s_pt;
-	char **arr_pt = malloc(n * sizeof(char*));
+	char **arr_pt = malloc(n * sizeof(char *));
 
 	if (arr_pt == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
-	
+
 	va_start(s_pt, n);
 	for (ind = 0; ind < n; ind++)
 	{
 		arr_pt[ind] = va_arg(s_pt, char*);
 	}
-	for(indx = 0; indx < n; indx++)
+	for (indx = 0; indx < n; indx++)
 	{
 		printf("%s", arr_pt[indx] != NULL ? arr_pt[indx] : "(nil)");
 		if (separator != NULL && indx != n - 1)
