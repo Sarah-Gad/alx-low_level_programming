fa3f6214bf696aec23ae189eb1c27399988d8c23
Sarah-Gad <sarahgad298@gmail.com>
2023-11-20 20:13:17 -0800

    completed task n.4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - a function that frees a listint_t list.
+ * @head: pointer to the fist node.
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *holder;
+	holder = head;
+	while (head != NULL)
+	{
+		holder = head->next;
+		free(head);
+		head = holder;
+	}
+}
