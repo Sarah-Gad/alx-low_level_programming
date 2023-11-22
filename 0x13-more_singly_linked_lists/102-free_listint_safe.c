d5d458e998bee5981088e79cf904cfaa7d779f27
Sarah-Gad <sarahgad298@gmail.com>
2023-11-21 18:30:13 -0800

    completed task n.13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,39 +1,76 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define node listint_t
+
 /**
- * free_listint_safe - a function to print element of list.
- * @h: pointer to the first node.
- * Return: size_t
+ * find_loop - finds a loop in a list
+ * @head: the head of the list
+ * Return: the node where the loop begins, or NULL
+ *		if there is no loop
 */
-size_t free_listint_safe(listint_t **h)
+node *find_loop2(node *head)
 {
-	size_t count = 0;
-	size_t i;
-	listint_t *ptr, *check, *temp;
+	node *slow, *fast;
 
-	ptr = *h;
-	while (*h) {
-		temp = *h;
-		*h = (*h)->next;
-		free(temp);
-		check = ptr;
-		count++;
+	if (!(head) || !(head->next))
+		return (NULL);
+	slow = head;
+	fast = head;
 
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-		for (i = 0; i < count; i++) {
-			if (*h == check) {
-
-				*h = NULL;
-				while (*h) {
-					temp = *h;
-					*h = (*h)->next;
-					free(temp);
-					count++;
-				}
-				return count;
+		if (slow == fast)
+		{
+			fast = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
 			}
-			check = check->next;
+			return (slow);
 		}
 	}
-	return count;
+	return (NULL);
 }
 
+/**
+ * free_listint_safe - prints all nodes in a linked list
+ * @h: the head of the list
+ * Return: the number of nodes in the list
+*/
+size_t free_listint_safe(node **h)
+{
+	node *mark, *temp, *head;
+	size_t size, i;
+	char seen = 0;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+	head = *h;
+	mark = find_loop2(head);
+	temp = head;
+
+	for (size = 0; temp != NULL; size++)
+	{
+		if (temp == mark)
+		{
+			if (seen)
+				break;
+			seen = 1;
+		}
+		temp = temp->next;
+	}
+	for (i = 0; i < size; i++)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	*h = NULL;
+	h = NULL;
+	return (size);
+}
