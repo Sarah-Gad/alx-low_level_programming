0d7e8510a17f1a6f55699288b8698033ee4c9378
Sarah-Gad <sarahgad298@gmail.com>
2023-11-27 15:40:52 -0800

    completed task n.1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,12 +14,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		f_ptr = open("filename", O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		f_ptr = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		if (f_ptr == -1)
 			return (-1);
 		return (1);
 	}
-	f_ptr = open("filename", O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	f_ptr = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (f_ptr == -1)
 		return (-1);
 	wttn = write(f_ptr, text_content, strlen(text_content));
