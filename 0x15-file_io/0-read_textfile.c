c3f030e48710eaac9ba864b7c8f49c6289ef4e55
Sarah-Gad <sarahgad298@gmail.com>
2023-11-27 10:54:04 -0800

    completed task n.0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,24 +11,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	size_t tot_num, rd_num;
 	FILE *f_ptr;
 	char *buff;
-
-	buff = malloc(sizeof(char) * (letters + 1));
-	if (buff == NULL)
+	
+	if (filename != NULL)
 	{
-		return (0);
-	}
-	f_ptr = fopen(filename, "r");
-	if (f_ptr == NULL)
-	{
+		buff = malloc(sizeof(char) * (letters + 1));
+		if (buff == NULL)
+		{
+			return (0);
+		}
+		f_ptr = fopen(filename, "r");
+		if (f_ptr == NULL)
+		{
+			free(buff);
+			return (0);
+		}
+		rd_num = fread(buff, sizeof(char), letters, f_ptr);
+		buff[rd_num] = '\0';
+		tot_num = write(1, buff, rd_num);
+		fclose(f_ptr);
 		free(buff);
-		return (0);
+		return (tot_num);
 	}
-
-	rd_num = fread(buff, sizeof(char), letters, f_ptr);
-	buff[rd_num] = '\0';
-	tot_num = write(1, buff, rd_num);
-	fclose(f_ptr);
-	free(buff);
-	return (tot_num);
+	else
+		return (0);
 }
 
