42a1d5e82499b19a27ddc05e9e632ac43077181a
Sarah-Gad <sarahgad298@gmail.com>
2024-01-11 18:21:02 -0800

    edit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,22 +12,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *moving, *moving2, *nw_nd;
 	unsigned int post = 1;
 
+	if (h == NULL)
+		return (NULL);
 	nw_nd = malloc(sizeof(dlistint_t));
 	if (nw_nd == NULL)
 		return (NULL);
+	if (idx == 0)
+	{
+		nw_nd->n = n;
+		nw_nd->prev = NULL;
+		nw_nd->next = *h;
+		if (*h != NULL)
+			(*h)->prev = nw_nd;
+		*h = nw_nd;
+		return (nw_nd);
+	}
 	moving = *h;
-	while (post < idx)
+	while (post < idx && moving != NULL)
 	{
 		moving = moving->next;
 		post++;
 	}
-	if (moving->next == NULL)
+	if (moving == NULL)
+	{
+		free(nw_nd);
 		return (NULL);
+	}
 	nw_nd->n = n;
 	moving2 = moving->next;
 	nw_nd->prev = moving;
 	moving->next = nw_nd;
 	nw_nd->next = moving2;
-	moving2->prev = nw_nd;
+	if (moving2 != NULL)
+		moving2->prev = nw_nd;
 	return (nw_nd);
 }
