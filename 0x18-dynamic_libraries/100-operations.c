f237264d93b576369e70200b6ad6ebc11a54dd40
Sarah-Gad <sarahgad298@gmail.com>
2024-01-15 16:11:10 -0800

    completed task n.2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+/**
+ * add - a function that adds tow ints.
+ * @x: the first int.
+ * @y:the second int.
+ * Return: the sum of thw two.
+*/
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
+/**
+ * sub -suntraction function.
+ * @x: fisrt int.
+ * @y: seconf int.
+ * Return: the sub.
+*/
+int sub(int x, int y)
+{
+	int sub = x - y;
+
+	return (sub);
+}
+/**
+ * mul - Multiplication func.
+ * @x: first int.
+ * @y: seconf int.
+ * Return: the mul.
+*/
+int mul(int x, int y)
+{
+	int mul = x * y;
+
+	return (mul);
+}
+/**
+ * div - division func
+ * @x: the fisr int
+ * @y:the second int
+ * Return: the div
+*/
+int div(int x, int y)
+{
+	int div = x / y;
+
+	return (div);
+}
+/**
+ * mod - modulu func
+ * @x: the first int
+ * @y: the seconf int
+ * Return: mod
+*/
+int mod(int x, int y)
+{
+	int mod = x % y;
+
+	return (mod);
+}
