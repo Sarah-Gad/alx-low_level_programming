d9415757c7c30c3fe8e86eb5a91564537aebf5d1
Sarah-Gad <sarahgad298@gmail.com>
2024-05-09 05:50:23 +0300

    0_0

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,91 +1,46 @@
 #include "search_algos.h"
-#include <stdio.h>
-#include <math.h>
 
 /**
- * node_at_index - returns the node at an index in a list
+ * linear_skip - Searches for an algorithm in a sorted singly
+ *               linked list of integers using linear skip.
+ * @list: A pointer to the  head of the linked list to search.
+ * @value: The value to search for.
  *
- * @list: a linked list
- * @index: the index at which to get the node
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
  *
- * Return: A pointer to the node at @index in @list
- */
-skiplist_t *node_at_index(skiplist_t *list, size_t index)
-{
-	skiplist_t *head = list, *temp = NULL;
-
-	if (index == 0)
-		return (list);
-
-	while (head && head->index < index)
-	{
-		temp = head;
-		head = head->express;
-		if (!head || head->index > index)
-		{
-			head = temp->next;
-			while (head && head != temp)
-			{
-				if (head->index == index)
-					return (head);
-				head = head->next;
-			}
-			break;
-		}
-		else if (head->index == index)
-			return (head);
-	}
-	return (NULL);
-}
-
-/**
- * linear_skip - searches for a node in a sorted skiplist of integers using
- * the jump search algorithm.
- *
- * @list: a linked list of integers
- * @value: the value to search for
- *
- * Return: a pointer to the node where @value is located or NULL if not found
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	size_t size, blcksize, low = 0, high = 0;
-	skiplist_t *target, *temp = list;
+	skiplist_t *node, *jump;
 
-	if (!list)
+	if (list == NULL)
 		return (NULL);
 
-	while (temp->express)
-		temp = temp->express;
-	while (temp->next)
-		temp = temp->next;
-	size = temp->index + 1;
-	blcksize = sqrt(size);
-
-	high = blcksize;
-	target = list->express;
-
-	while (target && target->n < value)
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
 	{
-		printf("Value checked array[%lu] = [%i]\n", high, target->n);
-		low = high;
-		high += blcksize;
-		target = target->express;
+		node = jump;
+		if (jump->express != NULL)
+		{
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+					jump->index, jump->n);
+		}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
 	}
 
-	high = MIN(high, size - 1);
-	target = node_at_index(list, high);
-	printf("Value checked array[%lu] = [%i]\n", high, target->n);
-	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
-	target = node_at_index(list, low);
-	for (; low < high && target->n < value; low++)
-	{
-		printf("Value checked array[%lu] = [%i]\n", low, target->n);
-		target = node_at_index(list, low + 1);
-	}
-	printf("Value checked array[%lu] = [%i]\n", low, target->n);
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
 
-	if (target->n == value)
-		return (target);
-	return (NULL);
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
 }
